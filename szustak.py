d45789845ed7e175cbd992ea335cfbf1cdc853cc
Author Name <klara.muzalewska@gmail.com>
2024-02-27 23:11:15 +0100

    Initial init of old repository with old game

---
+++ szustak.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pygame
+import numpy as np
+import random
+import os
+
+class Szustak(object):
+    def __init__(self, game):
+        self.current_path = os.path.dirname(__file__)
+        self.resource_path = os.path.join(self.current_path, 'pictures')
+        self.game = game
+        self.filename = self.resource_path + '/szustak.png'
+        # print self.filename
+        self.pos = np.array([random.uniform(0, 1160), random.uniform(0, 480)])
+        try:
+            self.sheet = pygame.image.load(self.filename).convert_alpha()
+        except pygame.error as message:
+            print('Unable to load spritesheet image:', self.filename)
+            raise(SystemExit, message)
+
+        self.size = self.sheet.get_rect().size
+
+    def draw(self):
+        self.game.screen.blit(self.sheet, (self.pos[0], self.pos[1]))
+
+    def collision(self):
+        if self.game.detectCollision(self):
+            self.game.move = False
